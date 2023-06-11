f88ef101ba2988b29bf494a1e1b169b250c44e1a
Марина Богданова <marinabogdanova@Marinas-MacBook-Air.local>
2023-06-11 12:42:31 +0300

    task2

--- solutions/task2.sh
+++ solutions/task2.sh
@@ -1,12 +1,24 @@
-echo “extracting public download link”
+echo Step 1/3: extracting public download link...
+
 curl https://cloud-api.yandex.net/v1/disk/public/resources/download\?public_key\=https://yadi.sk/d/ARJShvDUgazjMQ >> download_link.json
-URL="$(cat download_link.json | grep -o 'http[s]*:[^"]*')"
+download_link="$(cat download_link.json | grep -o 'http[s]*:[^"]*')"
 
-echo “start downloading data.json”
-curl -L -o "$URL" data.json
+echo \\n
+echo Step 2/3: start downloading data.json ...
+curl -L $download_link > ./clickhouse_data/user_files/data.json
 
-echo "deleting json file with public link"
-rm download_link.json
 
-echo "create table and insert data into table"
-echo '{"foo":"bar"}'  | clickhouse-client --query="CREATE TABLE users ENGINE=File(JSONEachRow, 'file.json')"
+# echo \nStep 3/3: create table and insert data into table ...
+# clickhouse-client --query="CREATE TABLE users ENGINE=File(JSONEachRow, 'data.json')"
+ 
+# echo "CREATE DATABASE test_uchi_ru" | curl -H 'X-ClickHouse-User: default' -H 'X-ClickHouse-Key: 12345' 'http://localhost:8123/' -d @-
+# echo "CREATE TABLE test_uchi_ru.users ENGINE=File(JSONEachRow, '/opt/data.json')" | curl -H 'X-ClickHouse-User: default' -H 'X-ClickHouse-Key: 12345' 'http://localhost:8123/' -d @-
+
+# rm download_link.json
+
+
+
+
+
+
+# CREATE TABLES users as SELECT * FROM file('./clickhouse-data/data.jsonl')
